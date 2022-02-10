87017de32a828185c17669790a454022024409ac
tythetasmaniantiger <99070833+tythetasmaniantiger@users.noreply.github.com>
2022-02-09 16:28:06 -0800

    Add files via upload

---
+++ ps0/MIT ps0.py
@@ -0,0 +1,15 @@
+"""
+Created on Wed Jan 26 23:10:26 2022
+
+@author: G.A.
+"""
+
+# Assignment 0
+
+import math   #math library needed for log
+
+x = int(input("Enter x value:"))     #prompt user input and convert to integer
+y = int(input("Enter y value:"))
+
+print("X**y","=",x**y)
+print("log(x)","=",math.log(x,2))
